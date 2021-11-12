abdba3a60c83fb8660995c6bf5606faa52437750
Cleggatron <david.robert.clegg@gmail.com>
2021-11-12 12:21:57 +0000

    updated the server file to use apollo

--- server/server.js
+++ server/server.js
@@ -2,13 +2,23 @@ const express = require('express');
 const path = require('path');
 const { ApolloServer } = require("apollo-server-express");
 
-//ToGo
-const routes = require('./routes');
-
 const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
+const {typeDefs, resolvers} = require("./schemas")
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+
+const server =  new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+})
+
+server.start().then(() => {
+  server.applyMiddleware({app})
+})
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,8 +28,14 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
